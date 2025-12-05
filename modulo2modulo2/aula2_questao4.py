0a5ac83c4f004117f68c14faafeb495c46c17549
DaviJose10 <davijose.santos@pditabira.com>
2025-12-05 12:48:30 -0300

    Add files via upload

---
+++ modulo2modulo2/aula2_questao4.py
@@ -0,0 +1,10 @@
+saldo = 500.0
+juros = 1.01
+
+# 3 meses acumulando juros
+saldo = saldo * juros
+saldo = saldo * juros
+saldo = saldo * juros
+
+print("Após 3 meses meu novo saldo é")
+print(saldo)
